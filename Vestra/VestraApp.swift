77463dfc786be2c44ef597064450e25d5f915400
Matthew Auciello <matt.auciello1999@gmail.com>
2025-11-02 19:30:48 +1100

    Initial Commit

---
+++ Vestra/VestraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VestraApp.swift
+//  Vestra
+//
+//  Created by Matthew Auciello on 2/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct VestraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VestraUITests/VestraUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VestraUITestsLaunchTests.swift
+//  VestraUITests
+//
+//  Created by Matthew Auciello on 2/11/2025.
+//
+
+import XCTest
+
+final class VestraUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
